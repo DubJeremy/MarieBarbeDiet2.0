1131c3a930d4b3350dba115757e8ca3b06beb271
DubJeremy <jeremy.ah33@gmail.com>
2022-06-20 18:30:17 +0200

    (WiP) setup

---
+++ components/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from "./home.module.scss";
+
+function Home() {
+    return (
+        <div className={styles.home}>
+            <h1>
+                yoyoyo
+            </h1>
+        </div>
+    );
+}
+
+export default Home;
