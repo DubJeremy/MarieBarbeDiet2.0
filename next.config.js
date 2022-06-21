9cc4f3d8226525cdfb7d67b2e32b6d5d2a4d03ef
DubJeremy <jeremy.ah33@gmail.com>
2022-06-21 16:44:21 +0200

    (Header): img png to svg + install svgr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  optimizeFonts: true,
+
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
