9970551e850fbfe8ae201ebd87a74d15d4e74386
DubJeremy <jeremy.ah33@gmail.com>
2022-06-22 10:42:55 +0200

    (Summary): content + style desktop

--- components/Program/index.tsx
+++ components/Program/index.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 
-import BackgroundVege from "../svgs/vegetables.svg";
-
 import styles from "./program.module.scss";
 
 function Program() {
     return (
         <div className={styles.program}>
-            <div className={styles.backgroundVege}>
-                <BackgroundVege />
-            </div>
+            
         </div>
     );
 }
