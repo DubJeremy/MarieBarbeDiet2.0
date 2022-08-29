f525cd30133cf35fed8c4fd30ea7cbab05453a00
DubJeremy <jeremy.ah33@gmail.com>
2022-08-29 20:02:58 +0200

    (Program): style

--- components/Program/index.tsx
+++ components/Program/index.tsx
@@ -9,44 +9,49 @@ function Program() {
         <div id="appointment" className={styles.program}>
             <h2>PROGRAMME</h2>
             <div className={`${styles.first} ${styles.appointment}`}>
-                <div className={styles.containerLeft}>
-                        <h4>Compter environ 1 heure</h4>
-                    <div className={styles.center}>
-                        <p>Bilan complet</p>
-                        <p>
-                            Mise en évidence des points positifs et des
-                            difficultés
-                        </p>
-                        <p>Définition du plan alimentaire personnalisé</p>
-                        <p>
-                            Mise en place du suivi pour préparer
-                            l&apos;accompagnement
-                        </p>
-                    </div>
+                <div className={`${styles.container} ${styles.left}`}>
                     <div className={styles.glassEffect}></div>
+                    <div className={styles.txt}>
+                        <h4>Compter environ 1 heure</h4>
+                        <div className={styles.center}>
+                            <p>Bilan complet</p>
+                            <p>
+                                Mise en évidence des points positifs et des
+                                difficultés
+                            </p>
+                            <p>Définition du plan alimentaire personnalisé</p>
+                            <p>
+                                Mise en place du suivi pour préparer
+                                l&apos;accompagnement
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div className={styles.containerRight}>
+                <div className={`${styles.title} ${styles.right}`}>
                     <h3>Première consultation / Bilan</h3>
                 </div>
             </div>
-            <div className={`${styles.next} ${styles.appointment}`}>
-                <div className={styles.containerLeft}>
+            <div className={`${styles.second} ${styles.appointment}`}>
+                <div className={`${styles.title} ${styles.left}`}>
                     <h3>Consultations de suivi</h3>
                 </div>
-                <div className={styles.containerRight}>
-                        <h4>Environ 30 min</h4>
-                    <div className={styles.center}>
-                        <p>Prise en compte de votre ressenti</p>
-                        <p>Mise en évidence de votre évolution</p>
-                        <p>
-                            Adaptation du plan alimentaire en fonction de vos
-                            besoins
-                        </p>
-                        <p>
-                            Réponses à vos questions et conseils personnalisés
-                        </p>
-                    </div>
+                <div className={`${styles.container} ${styles.right}`}>
                     <div className={styles.glassEffect}></div>
+                    <div className={styles.txt}>
+                        <h4>Environ 30 min</h4>
+                        <div className={styles.center}>
+                            <p>Prise en compte de votre ressenti</p>
+                            <p>Mise en évidence de votre évolution</p>
+                            <p>
+                                Adaptation du plan alimentaire en fonction de
+                                vos besoins
+                            </p>
+                            <p>
+                                Réponses à vos questions et conseils
+                                personnalisés
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
